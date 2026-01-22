f1a618f01c50d14d490f9bc4212356d11d7ab0af
Guillermo <gdominguezd06@informatica.iesvalledeljerteplasencia.es>
2026-01-22 16:58:29 +0100

    16:58

--- wp-task-manager.php
+++ wp-task-manager.php
@@ -11,3 +11,14 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Evita acceso directo al archivo
 }
+// Activación del plugin
+function wp_task_manager_activate() {
+    // Aquí podemos crear tablas en la base de datos
+    include_once plugin_dir_path( __FILE__ ) . 'includes/db.php';
+    wp_task_manager_create_table();
+}
+
+// Desactivación del plugin
+function wp_task_manager_deactivate() {
+    // Por ahora no haremos nada, pero aquí podríamos limpiar opciones
+}
