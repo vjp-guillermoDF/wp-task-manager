316f1d2f69b33346366e361c314c61b9c2306553
Guillermo <gdominguezd06@informatica.iesvalledeljerteplasencia.es>
2026-01-22 13:09:01 +0100

    Estructura inicial del plugin con archivo principal

---
+++ wp-task-manager.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: WP Task Manager
+ * Plugin URI: https://github.com/tuusuario/wp-task-manager
+ * Description: Plugin sencillo para practicar desarrollo backend en WordPress.
+ * Version: 1.0.0
+ * Author: Tu Nombre / Equipo
+ * License: GPL2
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Evita acceso directo al archivo
+}
